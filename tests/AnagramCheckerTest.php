c8871f47af75e2f9c494ddc3e32e9aa6dc6f77dc
Deron Johnson & Ashlin Aronin <student@epicodus.com>
2015-08-11 10:13:37 -0700

    Write first test and make it fail

---
+++ tests/AnagramCheckerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once "src/AnagramChecker.php"
+
+    class AnagramCheckerTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_checkAnagram_twoLetters()
+        {
+            //Arrange
+            $test_AnagramChecker = new AnagramChecker;
+            $input_word = "ab";
+            $guess_array = array("ba");
+
+            //Act
+            $result = $test_AnagramChecker->checkAnagram($input_word, $guess_array);
+
+            //Assert
+            $this->assertEquals(array("ba"), $result);
+
+        }
+    }
+?>
