92ddb384e7886f20b8ab39d65006703be275ba9e
Deron Johnson & Ashlin Aronin <student@epicodus.com>
2015-08-11 10:32:44 -0700

    Make first spec pass

--- src/AnagramChecker.php
+++ src/AnagramChecker.php
@@ -4,6 +4,29 @@
     {
         function checkAnagram($input_word, $guess_array)
         {
+            $input_word_array = str_split($input_word);
+            $output_array = array();
+
+            foreach ($guess_array as $word) {
+
+                $letter_array = str_split($word);
+                foreach($letter_array as $letter) {
+                    if (in_array($letter, $input_word_array)) {
+                        $key_of_input_letter = array_search($letter, $input_word_array);
+                        unset($input_word_array[$key_of_input_letter]);
+                    }
+                }
+
+                /* if the input word array is empty after looping through all
+                 letters of the word to test, then that word is an anagram
+                 and we should add it to the list. */
+                if (empty($input_word_array)) {
+                    array_push($output_array, $word);
+                }
+
+            }
+
+            return $output_array;
 
         }
     }
