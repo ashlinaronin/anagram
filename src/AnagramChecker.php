499fd306b5f673086bb709b776fe3a44f5265f77
Deron Johnson & Ashlin Aronin <student@epicodus.com>
2015-08-11 11:19:24 -0700

    Make original word in guesses test pass

--- src/AnagramChecker.php
+++ src/AnagramChecker.php
@@ -8,22 +8,23 @@
 
             foreach ($guess_array as $word) {
 
-                $input_word_array = str_split($input_word);
-                $letter_array = str_split($word);
-                foreach($letter_array as $letter) {
-                    if (in_array($letter, $input_word_array)) {
-                        $key_of_input_letter = array_search($letter, $input_word_array);
-                        unset($input_word_array[$key_of_input_letter]);
+                if ($word != $input_word) {
+                    $input_word_array = str_split($input_word);
+                    $letter_array = str_split($word);
+                    foreach($letter_array as $letter) {
+                        if (in_array($letter, $input_word_array)) {
+                            $key_of_input_letter = array_search($letter, $input_word_array);
+                            unset($input_word_array[$key_of_input_letter]);
+                        }
+                    }
+
+                    /* if the input word array is empty after looping through all
+                     letters of the word to test, then that word is an anagram
+                     and we should add it to the list. */
+                    if (empty($input_word_array)) {
+                        array_push($output_array, $word);
                     }
                 }
-
-                /* if the input word array is empty after looping through all
-                 letters of the word to test, then that word is an anagram
-                 and we should add it to the list. */
-                if (empty($input_word_array)) {
-                    array_push($output_array, $word);
-                }
-
             }
 
             return $output_array;
