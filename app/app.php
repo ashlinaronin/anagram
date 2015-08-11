2e4abca4986c8afaf41d144032004adffcfa5650
Deron Johnson & Ashlin Aronin <student@epicodus.com>
2015-08-11 12:09:16 -0700

    Modify app.php to match anagram twig form

--- app/app.php
+++ app/app.php
@@ -15,7 +15,7 @@
 
     $app->get("/anagrams", function() use($app) {
         $new_AnagramChecker = new AnagramChecker;
-        $results_array = $new_AnagramChecker->checkAnagram($_GET['input_word'], $_GET['guess_array']);
+        $results_array = $new_AnagramChecker->checkAnagram($_GET['input_word'], $_GET['guesses']);
         return $app['twig']->render('anagrams.html.twig', array('result' => $results_array));
     });
 
